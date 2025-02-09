82b11c229d4e5277bd5e87f2f6062279e9172182
abdelfatah hany <aaa5122003@gmail.com>
2025-02-09 07:31:14 +0200

    Update settings.py

--- library_system/library_system/settings.py
+++ library_system/library_system/settings.py
@@ -135,15 +135,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
-     'EXCEPTION_HANDLER': 'bookshelf.utils.custom_exception_handler',
 }
 
 
