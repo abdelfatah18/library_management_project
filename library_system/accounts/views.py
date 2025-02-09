9c31cb06074cd13c1d89c6b20d69ddd6ee390e15
abdelfatah hany <aaa5122003@gmail.com>
2025-02-09 07:33:53 +0200

    Update views.py

--- library_system/accounts/views.py
+++ library_system/accounts/views.py
@@ -5,15 +5,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 # عرض المستخدمين
 class UserListView(generics.ListCreateAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]   
+  
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 # عرض تفاصيل مستخدم واحد
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
