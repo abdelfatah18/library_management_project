558d47a7932419f58738505febb2c9a5a516fba7
abdelfatah hany <aaa5122003@gmail.com>
2025-02-09 07:33:17 +0200

    Update views.py

--- library_system/library/views.py
+++ library_system/library/views.py
@@ -5,13 +5,11 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class BookViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
