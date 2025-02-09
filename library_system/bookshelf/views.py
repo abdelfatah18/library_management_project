0b4ad5066ea404bb48cfd3048182c80314ab86a3
abdelfatah hany <aaa5122003@gmail.com>
2025-02-09 07:34:26 +0200

    Update views.py

--- library_system/bookshelf/views.py
+++ library_system/bookshelf/views.py
@@ -5,26 +5,22 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class BookshelfListCreateAPIView(generics.ListCreateAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    
     queryset = Bookshelf.objects.all()
     serializer_class = BookshelfSerializer
 
 class BookshelfRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Bookshelf.objects.all()
     serializer_class = BookshelfSerializer
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
